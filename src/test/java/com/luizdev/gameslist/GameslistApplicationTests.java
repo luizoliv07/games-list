e7b80d7058a29da5f5880510fbc00a7cf2358766
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-08-18 21:55:25 -0300

    Project started

---
+++ src/test/java/com/luizdev/gameslist/GameslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizdev.gameslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
